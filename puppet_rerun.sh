4ecf8f2127b18dbec78b63d757eecd7c59d1995d
Kai Pak <kai@kaipak.org>
2016-08-01 15:21:19 -0700

    Mostly completed script

--- puppet_rerun.sh
+++ puppet_rerun.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
-puppetrun_status="$(/bin/curl -sG https://master:8081/pdb/query/v4/nodes/`hostname` \
+puppetmaster="master.inf.puppet.vm"
+puppetagent_rc=1 # Priming a value so the while loop will run the first time
+puppetruns=0 # Keep track of how many runs we're doing
+failpoint=10 # Fail if number of runs reaches this number
+puppetrun_status="$(/bin/curl -sG https://$puppetmaster:8081/pdb/query/v4/nodes/`hostname` \
                    --tlsv1 \
                    --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem \
                    --cert /etc/puppetlabs/puppet/ssl/certs/`hostname`.pem \
                    --key /etc/puppetlabs/puppet/ssl/private_keys/`hostname`.pem \
                    --data-urlencode 'pretty=true' | awk -F\ :\  '/latest_report_status/ {print $2}' | sed s/\"//g)"
-puppetrun_exitcode=999
 
-# This will run infinitely!  Need to fix.  Find way to capture  exit code
-while [ $puppetrun_exitcode -ne 0 ]
-do
-  if [ PUPPET_RUN!='unchanged' ]
-  then
+# Loop through until Puppet run is successful and returns unchanged
+while [ "$puppetrun_status" != "unchanged" ]; do
+  # Loop through until agent run is successful.
+  while [ "$puppetagent_rc" -ne 0 ]; do
     /opt/puppetlabs/puppet/bin/puppet agent -t
-  fi
+    puppetagent_rc=$?
+    ((puppetruns++))
+    if [ "$puppetruns" -eq "$failpoint" ]; then
+      echo "Something is wrong, bailing script"
+      exit
+    fi
+  done
+  puppetrun_status="$(/bin/curl -sG https://master:8081/pdb/query/v4/nodes/`hostname` \
+                   --tlsv1 \
+                   --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem \
+                   --cert /etc/puppetlabs/puppet/ssl/certs/`hostname`.pem \
+                   --key /etc/puppetlabs/puppet/ssl/private_keys/`hostname`.pem \
+                   --data-urlencode 'pretty=true' | awk -F\ :\  '/latest_report_status/ {print $2}' | sed s/\"//g)"
 done
